b8efc8fe247b9f2d391c43fe3687fa49cc4f67d2
Sebastian Green-Husted <sgreenhusted@liquidweb.com>
2014-03-28 17:04:30 -0400

    add initial files

---
+++ src/fcache_app.erl
@@ -0,0 +1,16 @@
+-module(fcache_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    fcache_sup:start_link().
+
+stop(_State) ->
+    ok.
